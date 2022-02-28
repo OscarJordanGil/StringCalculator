6d3c768334976f00730fe2a50f59e2e505c65464
OscarJordan <oscarjordangil3@gmail.com>
2022-02-28 21:08:22 +0000

    Initial commit

--- src/Calculator.php
+++ src/Calculator.php
@@ -2,15 +2,87 @@
 
 namespace Deg540\PHPTestingBoilerplate;
 
-class Calculator
+class StringCalculator
 {
-    function add(int $number1, int $number2): int
+    function calcular(String $number): String
     {
-        return 0;
-    }
+        if (empty($number))
+        {
+            return "0";
+        }
+        else
+        {
+            $posicionerror = 1;
+            $total = 0.0;
+            $separador = "\n";
+            $separadasPorBarraN = explode($separador,$number);
+            $separadasPorSeparador = [];
+            $separadasTotal = [];
+            $numerosNegativos = [];
+            $arrayErrores = [];
 
-    function multiply(int $number1, int $number2): int
-    {
-        return 0;
+
+            $separador = substr($separadasPorBarraN[0],2);
+
+            if($number[strlen($number)-1] == $separador){
+                array_push($arrayErrores,'Number expected but NOT found ');
+
+            }
+            //print($separadasPorBarraN[1]);
+            $separadasPorSeparador = explode($separador,$separadasPorBarraN[1]);
+            foreach($separadasPorSeparador as $elem){
+                for($i=0;$i<strlen($elem);$i++){
+                    if(is_numeric($elem[$i]) or $elem[$i] == '-' or $elem[$i] == '.')
+                    {}else{
+                        $a = $separador." expected but ". $elem[$i] . " found at position " . $posicionerror;
+                        array_push($arrayErrores,$a);
+                        $elem[$i] = 'z';
+                    }
+                    $posicionerror++;
+                }
+                if (empty($elem)){
+                    array_push($arrayErrores,"Number expected but NOT found");
+                }
+                $sep = explode('z',$elem);
+                foreach($sep as $elem2){
+                    array_push($separadasTotal,$elem2);
+                }
+            }
+
+
+
+            //print($separador);
+
+
+            foreach ($separadasTotal as $elem){
+                $numeroAComprobarSiNegativo = (double)$elem;
+                if($numeroAComprobarSiNegativo<0){
+                    array_push($numerosNegativos,$numeroAComprobarSiNegativo);
+                }
+                else{
+                    $total += $numeroAComprobarSiNegativo;
+                }
+
+            }
+            if (empty($errores) and empty($numerosNegativos)){
+                return (String)$total;
+            }
+            else{
+                if(!empty($numerosNegativos))
+                {
+                    $devolverNumerosNegativos = "Negative not allowed : ";
+                    foreach($numerosNegativos as $elem){
+                        $devolverNumerosNegativos = $devolverNumerosNegativos .$elem .", ";
+                    }
+                    $errores = $devolverNumerosNegativos;
+                }
+                if(!empty($arrayErrores)) {
+                    foreach ($arrayErrores as $elem) {
+                        $errores = $errores . "\n" . $elem;
+                    }
+                }
+                return $errores;
+            }
+        }
     }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -7,29 +7,102 @@ namespace Deg540\PHPTestingBoilerplate\Test;
 use Deg540\PHPTestingBoilerplate\Calculator;
 use PHPUnit\Framework\TestCase;
 
-final class CalculatorTest extends TestCase
+final class StringCalculatorText extends TestCase
 {
     /**
      * @test
      */
-    public function shouldAddTwoArguments()
+    public function string_vacio_devuelve_0()
     {
-        $calculator = new Calculator();
+        $StringCalculator = new StringCalculator();
 
-        $result = $calculator->add(1, 2);
+        $result = $StringCalculator->calcular("");
 
-        $this->assertEquals(3, $result);
+        $this->assertEquals(0, $result);
     }
 
     /**
      * @test
      */
-    public function shouldMultiplyTwoArguments()
+    public function 1_devuelve_1()
     {
-        $calculator = new Calculator();
+        $StringCalculator = new StringCalculator();
 
-        $result = $calculator->multiply(1, 2);
+        $result = $StringCalculator->calcular("//,\n1");
 
-        $this->assertEquals(2, $result);
+        $this->assertEquals("1", $result);
+    }
+
+    /**
+     * @test
+     */
+    public function 1y2_devuelve_3()
+    {
+         $StringCalculator = new StringCalculator();
+
+         $result = $StringCalculator->calcular("//,\n1,2");
+
+         $this->assertEquals("3", $result);
+    }
+
+    /**
+     * @test
+     */
+    public function entre_separadores_no_hay_numero_error()
+    {
+        $StringCalculator = new StringCalculator();
+
+        $result = $StringCalculator->calcular("//,\n1,,2");
+
+        $this->assertEquals("Number expected but NOT found", $result);
+    }
+
+    /**
+     * @test
+     */
+    public function ultimo_valor_es_separador_error()
+    {
+        $StringCalculator = new StringCalculator();
+
+        $result = $StringCalculator->calcular("//,\n1,2,");
+
+        $this->assertEquals("Number expected but NOT found", $result);
+    }
+
+
+    /**
+     * @test
+     */
+    public function separador_incorrecto_error_separador()
+    {
+        $StringCalculator = new StringCalculator();
+
+        $result = $StringCalculator->calcular("//,\n1|2");
+
+        $this->assertEquals(", expected but | found at position 3", $result);
+    }
+
+    /**
+     * @test
+     */
+    public function numero_negativo_error()
+    {
+        $StringCalculator = new StringCalculator();
+
+        $result = $StringCalculator->calcular("//,\n1,-6");
+
+        $this->assertEquals("Negative not allowed : -6", $result);
+    }
+
+    /**
+     * @test
+     */
+    public function numerosos_errores_devuelven_varias_excepciones()
+    {
+        $StringCalculator = new StringCalculator();
+
+        $result = $StringCalculator->calcular("//,\n1,,2,-8");
+
+        $this->assertEquals("Number expected but NOT found\nNegative not allowed : -8", $result);
     }
 }
